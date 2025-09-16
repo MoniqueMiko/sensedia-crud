bb50f6e37c7c712804977a5e491b8626bd6e4a9c
Monique Ramos Lourenço <ext.mramos@spicbrasil.com.br>
2025-09-16 16:12:49 -0300

    new routes for test

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ let idAtual = 1;
 // Rota: Criar um novo usuário
 app.post('/usuarios', (req, res) => {
   const { nome, idade, profissao } = req.body;
-
   // Validação básica
   if (!nome || !idade || !profissao) {
     return res.status(400).json({ erro: 'Campos nome, idade e profissao são obrigatórios' });
@@ -29,9 +28,26 @@ app.post('/usuarios', (req, res) => {
   res.status(201).json(novoUsuario);
 });
 
+// Rota: edita um novo usuário
+app.put('/usuarios/:id', (req, res) => {
+  const { nome, idade, profissao } = req.body;
+  
+  // Validação básica
+  if (!nome || !idade || !profissao) {
+    return res.status(400).json({ erro: 'Campos nome, idade e profissao são obrigatórios' });
+  }
+
+  usuarios[Number(req.params.id) - 1] = { id: Number(req.params.id), nome: nome, idade: idade, profissao: profissao }
+  res.status(201).json(usuarios[Number(req.params.id) - 1]);
+});
+
 // Rota: Listar todos os usuários
 app.get('/usuarios', (req, res) => {
   res.json(usuarios);
+});
+
+app.get('/show/:id', (req, res) => {
+  res.json(usuarios[Number(req.params.id) - 1]);
 });
 
 app.listen(PORT, () => {
